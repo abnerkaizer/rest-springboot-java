96153b9631a624579655079dea22705c212281ac
Abner Kaizer <mail@abnerkaizer.com>
2025-07-24 14:58:20 -0300

    First steps in Java with Spring Boot 3.4.8

---
+++ src/main/java/com/abnerkaizer/rest_springboot_java/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.abnerkaizer.rest_springboot_java.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/test/java/com/abnerkaizer/rest_springboot_java/StartupTests.java
@@ -0,0 +1,13 @@
+package com.abnerkaizer.rest_springboot_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
