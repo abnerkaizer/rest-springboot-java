090d20bfed12cc3b64b0a8a4365b47ec1fc32690
Abner Kaizer <mail@abnerkaizer.com>
2025-12-15 09:58:01 -0300

    fix: added new information in the Authentication Swagger

--- src/main/java/com/abnerkaizer/rest_springboot_java/controllers/AuthController.java
+++ src/main/java/com/abnerkaizer/rest_springboot_java/controllers/AuthController.java
@@ -5,6 +5,8 @@ import com.abnerkaizer.rest_springboot_java.data.dto.security.AccountCredentials
 import com.abnerkaizer.rest_springboot_java.service.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +21,7 @@ public class AuthController implements AuthControllerDocs {
     @Autowired
     private AuthService service;
 
+    private Logger logger = LoggerFactory.getLogger(AuthController.class);
     @Override
     public ResponseEntity<?> signIn(@RequestBody AccountCredentialsDTO credentials){
 
@@ -26,6 +29,8 @@ public class AuthController implements AuthControllerDocs {
 
         var token = service.signIn(credentials);
         if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+
+        logger.info("User {} has logged in!", credentials.getUsername());
         return token;
     }
 
@@ -35,6 +40,7 @@ public class AuthController implements AuthControllerDocs {
 
         var token = service.refreshToken(username, refreshToken);
         if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        logger.info("Refresh Token has been used!");
         return token;
     }
 
@@ -52,6 +58,7 @@ public class AuthController implements AuthControllerDocs {
     )
     @Override
     public AccountCredentialsDTO create(@RequestBody AccountCredentialsDTO credentials){
+        logger.info("New user {} credentials has been created!", credentials.getUsername());
         return service.create(credentials);
     }
 
--- src/main/java/com/abnerkaizer/rest_springboot_java/controllers/docs/AuthControllerDocs.java
+++ src/main/java/com/abnerkaizer/rest_springboot_java/controllers/docs/AuthControllerDocs.java
@@ -3,9 +3,12 @@ package com.abnerkaizer.rest_springboot_java.controllers.docs;
 import com.abnerkaizer.rest_springboot_java.data.dto.PersonDTO;
 import com.abnerkaizer.rest_springboot_java.data.dto.security.AccountCredentialsDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -36,6 +39,16 @@ public interface AuthControllerDocs {
     @Operation(summary = "Refresh token for authenticated user and returns a token",
             description = "Refresh a access using their refresh token",
             tags = {"Authentication"},
+            security = @SecurityRequirement(name = "bearerAuth"),
+            parameters = {
+                @Parameter(
+                        name = "Authorization",
+                        description = "Bearer refresh token",
+                        required = true,
+                        in = ParameterIn.HEADER,
+                        example = "Bearer ey..."
+                )
+            },
             responses = {
                     @ApiResponse(
                             description = "Success",
